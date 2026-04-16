a716f452305d4d520304a78946166aa73badaf2a
Alvaro Molina <alvaro.molina@lindley.pe>
2026-04-16 11:44:03 -0500

    Se inicial el repositorio

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Windows Versions'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Text('Aplicación windows'),
+              Text('Versión')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
